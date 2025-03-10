a53951a3b6bef7fee3171e898f5b52b69726396b
NiruthmiAvindhya <niruthmi.chandrasekara@gmail.com>
2025-03-10 18:13:20 +0530

    Health Care Provider View _Medical History
    add the allergies page to the Health care Provider medical History view

--- Health care provider Medical History View/babyQ_frontend/app/_layout.tsx
+++ Health care provider Medical History View/babyQ_frontend/app/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="ChannelHistory" options={{ headerShown: false }} />
       <Stack.Screen name="AddChannelingHistory" options={{ headerShown: false }} />
+      <Stack.Screen name="AllergiesHistory" options={{ headerShown: false }} />
       {/* <Stack.Screen name="landingPage1" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="registerSelect" options={{ headerShown: false }} />
--- Health care provider Medical History View/babyQ_frontend/app/index.tsx
+++ Health care provider Medical History View/babyQ_frontend/app/index.tsx
@@ -13,9 +13,9 @@ const MedicalHistoryPage = () => {
   const goToChannelingHistory = () => {
     router.push('/ChannelHistory');
   };
-  // const goToAllergiesHistory = () => {
-  //   router.push('/AllergiesHistory');
-  // };
+  const goToAllergiesHistory = () => {
+    router.push('/AllergiesHistory');
+  };
  
 
   // const goToViewReports = () => {
@@ -50,14 +50,14 @@ const MedicalHistoryPage = () => {
       >
        Doctor Channelings
       </Button>
-      {/* <Button 
+      <Button 
         mode="contained" 
         onPress={goToAllergiesHistory}
         style={styles.button}
       >
        Allergies
       </Button>
-      <Button 
+      {/* <Button 
         mode="contained" 
         onPress={goToViewReports}
         style={styles.button}
