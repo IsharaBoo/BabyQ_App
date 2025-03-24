020f9964f6c189341a35cb9ebe04b1e13f018751
Dinulii <dinulihendawitharana03@gmail.com>
2025-03-24 08:52:01 +0530

    Update EditProfileScreen.tsx

--- dev/app/EditProfileScreen.tsx
+++ dev/app/EditProfileScreen.tsx
@@ -14,6 +14,7 @@ const getBackendUrl = () => {
     return 'http://10.0.2.2:8082'; // Emulator
   } else {
     return 'http://192.168.8.119:8082'; // iOS and physical devices
+    //else return 'http://10.31.23.48:8082';
   }
 };
 
